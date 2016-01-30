e8726857ccd7ee42c727895ed51f4dc1c438d074
Krzysztof Bojakowski <krzysztof0bojakowski@gmail.com>
2016-01-30 20:40:35 +0100

    Predictor added

--- EarleyParser/Program.cs
+++ EarleyParser/Program.cs
@@ -20,18 +20,18 @@ namespace EarleyParser
         }
         public class Grammar
         {
-            public Dictionary<List<string>, string> RulesKeyValues
-                = new Dictionary<List<string>, string>();
+            public Dictionary<List<List<string>>, string> RulesKeyValues
+                = new Dictionary<List<List<string>>, string>();
 
-            List<string> Values = new List<string>();
+            public List<string> Values = new List<string>();
 
-            public List<string> GetKeys(string left)
+            public List<List<string>> GetKeys(string left)
             {
-                List<string> right = null;
+                List<List<string>> right = null;
                 if (RulesKeyValues.ContainsValue(left))
-                    return RulesKeyValues
+                    return (List<List<string>>)RulesKeyValues
                         .Where(x => x.Value == left)
-                        .Select(x => x.Key).First();
+                        .Select(y=>y.Key.FirstOrDefault());
                 return right;
             }
 
@@ -41,27 +41,81 @@ namespace EarleyParser
             }
         }
 
+        public const string DOT = "@";
         public class State
         {
-            public string left;
-            public List<string> right;
+            int dotIndex = -1;
+
+            public string termsLeft;
+            public List<string> termsRight;
             public int i, j;
+
+            public string GetTermAfterDot()
+            {
+                if (dotIndex != -1 && dotIndex < termsRight.Count() - 1)
+                    return termsRight[dotIndex];
+                return String.Empty;
+            }
+
+            public bool IsDotIsAtTheEnd()
+            {
+                if (termsRight[termsRight.Count() - 1] == DOT)
+                    return true;
+                return false;
+            }
+
+            public bool IsTermPartOfSpeech(string term, Grammar grammar)
+            {
+                if (grammar.Values.Contains(term))
+                    return true;
+                return false;
+            }
         }
 
         public class EarleyParser
         {
+            public void Predictor(State state)
+            {
+                var termsLeft = state.GetTermAfterDot();
+                foreach (var term in Grammar.GetKeys(termsLeft))
+                {
+                    Charts[state.j]
+                        .AddToChart(new State()
+                        {
+                            termsLeft = termsLeft,
+                            termsRight = term,
+                            i = state.j,
+                            j = state.j
+                        });
+                }
+            }
+
+            public List<string> Words;
+            public Grammar Grammar;
+            public List<Chart> Charts;
+
             public void Parse(List<string> words, Grammar grammar)
             {
-                List<Chart> charts = new List<Chart>(words.Count());
+                Grammar = grammar;
+                this.Words = words;
+                Charts = new List<Chart>(Words.Count());
                 State dummyStartState = 
-                    new State() { left = "$", right = new List<string>() { "@", "S" }, i = 0, j = 0 };
-                charts[0].AddToChart(dummyStartState);
+                    new State() { termsLeft = "$", termsRight = new List<string>() { DOT, "S" }, i = 0, j = 0 };
+                Charts[0].AddToChart(dummyStartState);
 
-                for (int i = 0; i < words.Count(); ++i)
+                for (int i = 0; i < Words.Count(); ++i)
                 {
-                    foreach (var state in charts[i].chart)
+                    foreach (var state in Charts[i].chart)
                     {
-
+                        var termAfterDot = state.GetTermAfterDot();
+                        if (!string.IsNullOrEmpty(termAfterDot) && !state.IsTermPartOfSpeech(termAfterDot, Grammar))
+                            Predictor(state);
+                        else if (!string.IsNullOrEmpty(termAfterDot) && state.IsTermPartOfSpeech(termAfterDot, Grammar))
+                            throw new NotImplementedException();
+                        //scanner(state)
+                        else
+                            throw new NotImplementedException();
+                        //completer(state)
                     }
                 }
             }
